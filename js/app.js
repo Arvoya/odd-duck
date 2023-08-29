c051ec58db687149c50eabad171e5c0147e3046d
Isai Chaidez <isaichaidez@gmail.com>
2023-08-29 11:49:36 -0700

    updated layout, created all TODO for JS

--- js/app.js
+++ js/app.js
@@ -1,2 +1,30 @@
 'use strict';
 
+/**
+ * TODO Create Constructor function
+ * TODO Constructor properties: Name, File path, Time image has been shown
+ * TODO Create helper function that generates 3 unique product images from img directory
+ * TODO Every time it has been shown count it
+ * TODO Attach Event listener to the section of the HTML page where the images are going to be displayed
+ * TODO Once user clicks a product, generate three new products
+ * TODO Keep track of number of times a product has been clicked 
+ * TODO Update the count on the results section in HTML
+ * TODO User should be presented with 25 rounds of voting
+ * TODO keep the rounds in a variable to allow the number to easily changed
+ * TODO Create a property attached to the constructor function to track which products have been clicked.
+ * TODO Add a button with the text view results
+ * TODO button when clicked list of all the products followed by the votes received, and the number of times seen for each. example "banana had 3 votes, and was seen 5 times."
+ */
+
+// *GLOBALS
+
+// * DOM WINDOWS
+
+// * CONSTRUCTOR FUNCTIONS
+
+
+// * HELPER FUNCTIONS
+
+// * EVENT HANDLERS
+
+// * EXECUTABLE CODE
